c49ac84a4576e18a3d9f2a678ca351a1215f6bb4
A91y <65825207+A91y@users.noreply.github.com>
2024-01-01 23:21:01 +0530

    💡Added login and register logic

--- app/views.py
+++ app/views.py
@@ -1,32 +1,78 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
+from .utils import is_valid_email
 # Create your views here.
 
-def index(request: HttpRequest):
-    return render(request, "index.html", context={"name": "index"})
+
+def home(request: HttpRequest):
+    return render(request, "home.html", context={"name": "home"})
+
 
 def login_view(request: HttpRequest):
     if request.user.is_authenticated:
-        return redirect("index")
+        messages.info(request, "Already, logged in...")
+        return redirect("home")
     if request.method == "POST":
         email: str = request.POST.get("email", "")
-        password: str = request.POST.get("password", "") 
+        password: str = request.POST.get("password", "")
+
+        if not email or not password:
+            messages.error(request, "Please fill out all required fields...")
+            return redirect("login")
+
+        if not is_valid_email(email):
+            messages.error(request, "Please provide a valid email address...")
+            return redirect("login")
+
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Successfully! Logged in.")
+            return redirect("home")
+        messages.error(request, "Incorrect Email or Password...")
+        return redirect("login")
+
     return render(request, "login.html", context={"name": "login"})
+
 
 def register_view(request: HttpRequest):
     if request.user.is_authenticated:
-        return redirect("index")
+        messages.info(request, "Already, logged in...")
+        return redirect("home")
     if request.method == "POST":
-        first_name: str = request.POST.get("fname", "")
+        first_name: str = request.POST.get("fname", None)
         last_name: str = request.POST.get("lname", "")
-        email: str = request.POST.get("email", "")
-        password: str = request.POST.get("password", "")
+        email: str = request.POST.get("email", None)
+        password: str = request.POST.get("password", None)
+        if not first_name or not email or not password:
+            messages.error(request, "Please fill out all required fields...")
+            return redirect("register")
+        if not is_valid_email(email):
+            messages.error(request, "Please provide a valid email address...")
+            return redirect("register")
+        if User.objects.filter(email=email).exists():
+            messages.info(request, "User with this email already exists...")
+            return redirect("home")
+        user = User.objects.create_user(
+            username=email, email=email, password=password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+
+        user = authenticate(request, username=email, password=password)
+        login(request, user)
+        messages.success(request, "User registered successfully...")
+        return redirect("home")
     return render(request, "register.html", context={"name": "register"})
+
 
 @login_required
 def logout_view(request: HttpRequest):
     logout(request)
-    return redirect("index")
+    messages.success(request, "Logged out...")
+    return redirect("home")
