21e24a09ca4f546030434a0098a880b0b6c38e9a
A91y <65825207+A91y@users.noreply.github.com>
2024-01-01 21:41:05 +0530

    🚀Add login, register, and logout views Update ALLOWED_HOSTS and DIRS settings Add LOGIN_URL to settings Include app.urls in urlpatterns Add start script to pyproject.toml

--- app/views.py
+++ app/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 # Create your views here.
+
+def index(request: HttpRequest):
+    return render(request, "index.html", context={"name": "index"})
+
+def login_view(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect("index")
+    if request.method == "POST":
+        email: str = request.POST.get("email", "")
+        password: str = request.POST.get("password", "") 
+    return render(request, "login.html", context={"name": "login"})
+
+def register_view(request: HttpRequest):
+    if request.user.is_authenticated:
+        return redirect("index")
+    if request.method == "POST":
+        first_name: str = request.POST.get("fname", "")
+        last_name: str = request.POST.get("lname", "")
+        email: str = request.POST.get("email", "")
+        password: str = request.POST.get("password", "")
+    return render(request, "register.html", context={"name": "register"})
+
+@login_required
+def logout_view(request: HttpRequest):
+    logout(request)
+    return redirect("index")
--- django_simple_auth/settings.py
+++ django_simple_auth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n3$zj+3(hs-b^pp+f#i)x+%ul3(8d!q@p6o(pqi-te8#ustxn$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'django_simple_auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL='/login'
